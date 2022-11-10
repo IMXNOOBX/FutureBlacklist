0d95ab22f749d78c954d5d937fb0323ac51dccc7
ITSXNOOBX <arangojond@gmail.com>
2022-11-10 14:20:14 +0100

    1.0.3 - Upgraded api

---
+++ rest-api/exist_user.bat
@@ -0,0 +1,3 @@
+:loop 
+curl "http://127.0.0.1/api/v1/user/exist/8167293698" 
+goto loop 
--- rest-api/flood2.bat
+++
@@ -1,3 +0,0 @@
-:loop 
-curl "http://127.0.0.1/api/v1/insert?rid=%random%&name=elticonstory&ip=&note=Normal+player.&modder=0" 
-goto loop 
