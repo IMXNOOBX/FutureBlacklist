8c53c524b3c8c9589419a7ba38ce6d9c9e95ebe6
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-11-18 14:09:21 +0100

    Fixed stand async request responses

--- nodejs/routes/v0.js
+++ nodejs/routes/v0.js
@@ -1,7 +1,25 @@
-exports.insert = function(req, res) {
-    
+exports.get_user = function(req, res) {
+    var db = req.app.get('db');
+    res.send({
+        params: req.params.rid,
+        success: true,
+    })
 };
 
-exports.get_user = function(req, res) {
-
+exports.insert = function(req, res) {
+    var db = req.app.get('db');
+    let key = req.query.key
+    let rid = req.query.rid
+    let name = req.query.name
+    let ip = req.query.ip
+    let note = req.query.note
+    let modder = (req.query.modder && (req.query.modder.toLowerCase() == "true" || req.query.modder.toLowerCase() == "1")) ? 1 : 0
+    let advertiser = (req.query.advertiser && (req.query.advertiser.toLowerCase() == "true" || req.query.advertiser.toLowerCase() == "1")) ? 1 : 0
+    let risk = req.query.risk
+    let date = Date.now();
+    
+    res.send({
+        params: req.params.rid,
+        success: true,
+    })
 };
