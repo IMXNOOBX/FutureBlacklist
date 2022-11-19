74fd3e313cde4e0feb226e7f8a14c329c1b3cd39
IMXNOOBX <paraelmobil403@gmail.com>
2022-11-19 21:44:16 +0100

    asd

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import discord
 from discord import app_commands
-from discord.ext import commands
-# from discord.commands.context import ApplicationContext
-# from discord.commands import Option
 import mysql.connector
-
-bot = commands.Bot(command_prefix="!")
-
-con = mysql.connector.connect(
-  host="localhost",
-  user="Future5",
-  password="GNB82hBjtA54P8ey",
-  database="future5"
-)
 
 adm_user_id = '652969127756955658'
 
-@bot.slash_command(name="first_slash")
-async def first_slash(ctx): 
-    await ctx.respond("You executed the slash command!")
+# con = mysql.connector.connect(
+#   host="localhost",
+#   user="Future5",
+#   password="GNB82hBjtA54P8ey",
+#   database="future5"
+# )
+
+intents = discord.Intents.default()
+client = discord.Client(command_prefix="!", help_command=None, intents=intents, guild_ids=[749963611807547442])
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie")) # https://stackoverflow.com/a/59126629/15384495
+    print("bot Ready!")
 
 
-# @bot.slash_command(name="genkey", description= "Generate api key for a new user")
-# async def first_slash(
-# 	ctx: discord.ApplicationContext,
-#     name: Option(str, "Enter your name")
-# 	): 
-#     await ctx.respond("You executed the slash command!")
+@tree.command(name="help", description="Some help about this bot.")
+async def help_command(interaction):
+    await interaction.response.send_message("Hello!")
 
-bot.run('MTA0MzMxNDk1NDE4MDIzOTQ1Ng.G1ND_r.QoMsQBpwlNoZ39dAwBuLnslQ4ribggVgQkLqIc')
+@tree.command(name="status", description="Get bot status.")
+async def status_command(interaction):
+    await interaction.response.send_message("Status!")
+
+
+key_group = app_commands.Group(
+    name="key", description="API Key related commands. (Owner only)")
+
+
+@key_group.command(name="generate", description="Generate api key for a new user")
+async def apikey_command(interaction):
+    await interaction.response.send_message("You executed the slash command!", ephemeral=True)
+
+client.run(
+    'MTA0MzMxNDk1NDE4MDIzOTQ1Ng.G1ND_r.QoMsQBpwlNoZ39dAwBuLnslQ4ribggVgQkLqIc')
